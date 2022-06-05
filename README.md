ca81f848a4faa8ebbc06175dbad58d16f1ef8fad
saunack <kr.saunack@gmail.com>
2022-06-05 14:22:02 +0530

    add sample outputs, modify args

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
-[PDF source](https://algorithmsbook.com/optimization/files/optimization.pdf) under Creative Commons License
+Handbook on optimization algorithms by MIT Press: [PDF](https://algorithmsbook.com/optimization/files/optimization.pdf) under Creative Commons License
 
 ### Simulated Annealing
-https://en.wikipedia.org/wiki/Metropolis%E2%80%93Hastings_algorithm
+On running main,py, graphs are generated for the objective function defined in `annealing/function.py` for the following:
+- GIF of candidates and objective function as temperature changes
+- Temperature vs iterations
+- Acceptance probability vs iterations
+- Objective function vs iterations
+- Plot of the objective function
 
+Initialization and configuration options are available in `main.py`
--- annealing/main.py
+++ annealing/main.py
@@ -8,25 +8,30 @@ parser.add_argument('--disable_gif', dest='base_plot', action='store_false',
 						help='flag for disabling gif creation')
 parser.add_argument('--prefix', default="sa_",
                     help='add file prefix names')
-parser.add_argument('--output_dir', default=".",
+parser.add_argument('--output_dir', default="output/",
                     help='output_directory')
 parser.add_argument('--x0', default=0, type=float,
-                    help='output_directory')
+                    help='initial solution')
 parser.add_argument('--t0', default=20, type=float,
-                    help='output_directory')
+                    help='initial temperature')
 parser.add_argument('--decay', default=0.92, type=float,
-                    help='output_directory')
+                    help='decay factor for temperature')
 parser.add_argument('--thresh', default=0.1, type=float,
-                    help='output_directory')
+                    help='threshold for temperature')
+parser.add_argument('--iterations', default=None, type=int,
+                    help='Number of iterations to run the code for (threshold is ignored if iterations are provided)')
 
 args = parser.parse_args()
 
-def run_annealing_quadratic(x, T, decay=0.8, threshold=0.1, base_plot=False):
-	run_logs = annealing(f, sampler, x, T, decay=decay, threshold=threshold, base_plot=base_plot)
+def run_annealing_quadratic(x, T, decay, threshold, iterations, base_plot):
+	if iterations is not None:
+		run_logs = annealing(f, sampler, x, T, decay=decay, iterations=iterations, base_plot=base_plot)
+	else:
+		run_logs = annealing(f, sampler, x, T, decay=decay, threshold=threshold, base_plot=base_plot)
 	return run_logs
 
 if __name__ == "__main__":
 	x_opt, run_logs, (x,y) = run_annealing_quadratic(args.x0,args.t0,decay=args.decay,
-										 threshold=args.thresh, base_plot=args.base_plot)
+										 threshold=args.thresh, iterations=args.iterations, base_plot=args.base_plot)
 	print("Optimal solution: ",x_opt)
 	plot(run_logs,args.output_dir,args.prefix,base_plot=(x,y))
